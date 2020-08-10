e02c31f8beae7188f36d44a362eaf894a1b709d0
anzhaurova <anzhaurova.v@gmail.com>
2020-08-10 23:52:22 +0300

    add bootstrap

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
-<div class = "header">
+
+<div class = "footer">
     <div class="container" style="text-align: center; background-color: lightblue">
         <div class="logo">Здесь будет footer</div>
+        <div class="col-12"></div>
     </div>
 </div>
+<script src="/assets/bootstrap_3/js/bootstrap.min.js"></script>
--- header.php
+++ header.php
@@ -1,5 +1,11 @@
+<head>
+    <link rel="stylesheet" href="/assets/bootstrap_3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/bootstrap_3/css/bootstrap-theme.min.css">
+</head>
 <div class = "header" style="text-align: center; background-color: lightgreen">
     <div class="container">
         <div class="logo">Здесь будет header</div>
     </div>
 </div>
+
+
